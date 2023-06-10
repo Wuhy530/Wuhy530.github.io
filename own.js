38d2457eeaab843ca4f5b379c0cdb60eb8db5ff1
Wuhy530 <126553774+Wuhy530@users.noreply.github.com>
2023-06-10 14:59:13 +0800

    Add files via upload

---
+++ own.js
@@ -0,0 +1,60 @@
+function scene(){
+  let pg1=document.querySelector("#food")
+  pg1.style.display="none"
+  let pg2=document.querySelector("#person")
+  pg2.style.display="none"
+  let pg3=document.querySelector("#unperson")
+  pg3.style.display="none"
+  let pg4=document.querySelector("#scene")
+  pg4.style.display="block"
+}
+function food(){
+let pg1=document.querySelector("#food")
+pg1.style.display="block"
+let pg2=document.querySelector("#person")
+pg2.style.display="none"
+let pg3=document.querySelector("#unperson")
+pg3.style.display="none"
+let pg4=document.querySelector("#scene")
+pg4.style.display="none"
+}
+function live(){
+let pg1=document.querySelector("#food")
+pg1.style.display="none"
+let pg2=document.querySelector("#person")
+pg2.style.display="block"
+let pg3=document.querySelector("#unperson")
+pg3.style.display="block"
+let pg4=document.querySelector("#scene")
+pg4.style.display="none"
+}
+function person(){
+let pg1=document.querySelector("#food")
+pg1.style.display="none"
+let pg2=document.querySelector("#person")
+pg2.style.display="block"
+let pg3=document.querySelector("#unperson")
+pg3.style.display="none"
+let pg4=document.querySelector("#scene")
+pg4.style.display="none"
+}
+function unperson(){
+let pg1=document.querySelector("#food")
+pg1.style.display="none"
+let pg2=document.querySelector("#person")
+pg2.style.display="none"
+let pg3=document.querySelector("#unperson")
+pg3.style.display="block"
+let pg4=document.querySelector("#scene")
+pg4.style.display="none"
+}
+
+function trytoanswer1(){
+  alert("疫情居家锻炼期间，您是否进行运动前热身或运动后拉伸");
+  }
+function trytoanswer2(){
+  alert("您对IP、虚拟人物/虚拟偶像、元宇宙的感兴趣程度是怎样的");
+  }
+function trytoanswer3(){
+    alert("你知道种植矮化品种果树有什么好处嘛");
+    }
